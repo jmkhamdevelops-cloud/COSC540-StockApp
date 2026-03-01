6add8a0125b72d99c08bf11d58d069d675a3b2e6
jmkhamdevelops-cloud <jmkhamdevelops@gmail.com>
2026-03-01 14:29:03 -0600

    Modified files to match tutorial

--- Backend/config/wsgi.py
+++ Backend/config/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+settings_module = 'config.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'config.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
